d6fe31d58fefb31dbb664e32fbdaf7c995bceda2
Anwar_Hossain <ranakrphone@gmail.com>
2024-12-04 20:13:16 +0600

    movie deletion done

--- index.js
+++ index.js
@@ -75,6 +75,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/allMovies/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await movieCollection.deleteOne(query);
+      res.send(result);
+    });
+
     //---------------------------- Movies related apis end
 
     //---------------------------- Users related apis start
